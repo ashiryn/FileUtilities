ca40a1aca151386190d49c82d0cce8fecab9e78f
ashiryn <ashiryn@users.noreply.github.com>
2026-05-11 19:10:32 -0400

    Migrating code to meet personal coding standards (#10)

---
+++ FluffyVoid.FileUtilities/DataLoaderPaths/NextToExecutablePath.cs
@@ -0,0 +1,25 @@
+﻿namespace FluffyVoid.FileUtilities
+{
+    /// <summary>
+    ///     Data path definition to retrieve files from a location alongside the
+    ///     running executable
+    /// </summary>
+    public class NextToExecutablePath : IDataLoaderPath
+    {
+        /// <summary>
+        ///     Retrieves the data path to use for file IO
+        /// </summary>
+        /// <param name="dataLocation">The data location to get a file path for</param>
+        /// <returns>The file path to use for file IO</returns>
+        public string GetDataPath(DataLocation dataLocation)
+        {
+            return dataLocation switch
+            {
+                DataLocation.ApplicationData => "/Data",
+                DataLocation.UserData        => "/Data/User",
+                DataLocation.SaveData        => "/Data/Saves",
+                _                            => "/Data",
+            };
+        }
+    }
+}
