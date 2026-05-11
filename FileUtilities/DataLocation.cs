861bcc6d013e44bdf3ab2052845370650b5b7002
Joe Thompson <azyle0823@gmail.com>
2026-05-11 19:05:34 -0400

    Aligning with personal coding standards

--- FileUtilities/DataLocation.cs
+++ FileUtilities/DataLocation.cs
@@ -1,20 +1,21 @@
-namespace FluffyVoid.FileUtilities;
-
-/// <summary>
-///     The types of data storage locations the data loader supports
-/// </summary>
-public enum DataLocation
+namespace FluffyVoid.FileUtilities
 {
     /// <summary>
-    /// Location used to store application specific data and/or settings
+    ///     The types of data storage locations the data loader supports
     /// </summary>
-    ApplicationData,
-    /// <summary>
-    /// Location used to store user specific data and/or settings
-    /// </summary>
-    UserData,
-    /// <summary>
-    /// Location used to store save data for an application session
-    /// </summary>
-    SaveData
+    public enum DataLocation
+    {
+        /// <summary>
+        ///     Location used to store application specific data and/or settings
+        /// </summary>
+        ApplicationData,
+        /// <summary>
+        ///     Location used to store user specific data and/or settings
+        /// </summary>
+        UserData,
+        /// <summary>
+        ///     Location used to store save data for an application session
+        /// </summary>
+        SaveData,
+    }
 }
